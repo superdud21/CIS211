8aad3243e6b4caa929455e534dba5e6f498e3af9
Caleb Cameron <calebcam21@gmail.com>
2018-02-12 19:05:12 -0500

    Wrote 0301

--- lab03/lab03_01/lab03_01/Source.cpp
+++ lab03/lab03_01/lab03_01/Source.cpp
@@ -1,1 +1,31 @@
-//test
+//Caleb Cameron-PC03-01
+//The purpose of this program is to calculate miles per gallon of a vehicle with user input
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Declare variables
+	double miles;
+	double gallons;
+	double milesPerGallon;
+	
+	//Display questions and get user input
+	cout << "Enter number of miles traveled: " << endl;
+	cin >> miles;
+	cout << "Enter number of gallons of gas used: " << endl;
+	cin >> gallons;
+
+	//Calculate Miles Per Gallon
+	milesPerGallon = miles / gallons;
+
+	//Display result
+	cout << "The miles per gallon during travel was: " << milesPerGallon << endl;
+	system("pause");
+	return 0;
+	
+
+
+
+}
