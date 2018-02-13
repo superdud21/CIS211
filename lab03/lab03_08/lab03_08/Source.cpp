2d52c3dc59af6b0de754a77d4b8d49d01ccc9e23
Caleb Cameron <calebcam21@gmail.com>
2018-02-12 22:50:50 -0500

    Wrote lab03_08
    Calculating percentages

---
+++ lab03/lab03_08/lab03_08/Source.cpp
@@ -0,0 +1,35 @@
+/*Caleb Cameron lab03_08
+This program is to calculate the percentage of males and females in a registered class.
+The values should add up to 100% allowed 2 decimal places.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	// Declare variables
+	float males, females, total, numMales, numfemales, mpercent, fpercent;
+
+	//User input
+	cout << "How many males are in the class? ";
+	cin >> males;
+	cout << "How many females are in the class? ";
+	cin >> females;
+
+	//Calculations
+	total = males + females;
+
+	numMales = males / total;
+	numfemales = females / total;
+	mpercent = numMales * 100;
+	fpercent = numfemales * 100;
+
+	//Dislay results in percent form
+	cout << fixed << setprecision(2) << "Percent of males is: " << mpercent << "%" << endl;
+	cout << fixed << setprecision(2) << "Percent of females is: " << fpercent << "%" << endl;
+
+	system("pause");
+	return 0;
+}
